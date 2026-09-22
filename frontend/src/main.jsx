a3ac23c8dccbaa5da6933ee28255c1225be0cb24
Kabbilan <kabbilan5608@gmail.com>
2026-09-22 09:48:54 +0530

    Enable expanded SentraPixel SOC workspace

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SocShell from "./SocShell";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SocShell>
+      <App />
+    </SocShell>
   </React.StrictMode>
 );
-
